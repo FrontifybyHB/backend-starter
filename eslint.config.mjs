8e689c5eea55775273a9f51e6f93366856dcef8d
Himanshu <work.himanshu.hb@gmail.com>
2025-12-22 22:16:38 +0530

    Backend Starter eslint file mjs update v1.3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,35 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import eslintPluginImport from 'eslint-plugin-import';
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
-  
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser
+    },
+    plugins: {
+      js,
+      import: eslintPluginImport,
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.mjs', '.cjs', '.json'],
+        },
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'import/order': [
+        'warn',
+        { groups: [['builtin', 'external', 'internal']] },
+      ],
+      'import/no-unresolved': 'error',
+    },
+  },
+];
